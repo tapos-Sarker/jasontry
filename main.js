feaece67c6841b1d8bdb33f1064b4b07d41245fb
taposSarker <tsarker2595@gmail.com>
2018-04-16 21:20:52 +0600

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-main.js
+var ourreq = new XMLHttpRequest();
+ourreq.open('GET','https://github.com/tapos-Sarker/jasontry/blob/master/datas.json')
+ourreq.onload = function(){
+	 console.log(ourreq.responseText);
+};
+ourreq.send();
